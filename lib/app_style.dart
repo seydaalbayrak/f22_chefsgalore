3db29d79d53ac971eba2edd4d59cfbddbb56274d
Ibrahim Balta <baltaibrahim584@gmail.com>
2023-06-12 21:38:45 +0300

    main update

--- lib/app_style.dart
+++ lib/app_style.dart
@@ -2,42 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const Color sWhite = Color(0xffffffff);
-const Color sSecondary = Color(0xffFFFFFF);
-
+const Color sPrimary = Color(0xff129575);
+const Color sSecondary = Color(0xffFF9C00 );
+const Color sRating = Color(0xffFFAD30);
 const Color sBlack = Color(0xff111111);
-const Color sNavigatoinClick = Colors.black;
-const Color sGrey = Color.fromARGB(255, 112, 112, 112);
-const Color sPrimary = Color(0xffFFB347);
 const Color sBackground =Color(0xffF1F1F1);
 
-List<Color> cardColor =[
-Colors.white,
-Colors.red.shade100,
-Colors.orange.shade100,
-Colors.green.shade100,
-Colors.blue.shade100,
-Colors.yellow.shade100,
-Colors.pink.shade100,
-Colors.blueGrey.shade100,
+const Color sSuccess = Color(0xff31B057);
+const Color sNavigatoinClick = Colors.black;
 
-];
 
-const double sBorderRadius = 100.0;
+const double sBorderRadius = 10.0;
 const double sPaddinHorizontal = 22.0;
 
 
-final sEncodeSansBold = GoogleFonts.encodeSans(
+final sPoppinsBold = GoogleFonts.poppins(
     fontWeight: FontWeight.w700
 );
 
-final sEncodeSansSemiBold = GoogleFonts.encodeSans(
+final sPoppinsSemiBold = GoogleFonts.poppins(
     fontWeight: FontWeight.w600
 );
 
-final sEncodeSansMedium = GoogleFonts.encodeSans(
+final sPoppinsMedium = GoogleFonts.poppins(
     fontWeight: FontWeight.w500
 );
 
-final sEncodeSansRegular = GoogleFonts.encodeSans(
+final sPoppinsRegular = GoogleFonts.poppins(
     fontWeight: FontWeight.w400
 );
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:f22_chefsgalore/app_style.dart';
 import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}):super(key:key);
@@ -10,16 +11,16 @@ class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: sBackground,
       bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.white,
-        color:Color(0xff129575),
-        items:[
-          Icon(Icons.home,color:Colors.white),
-          Icon(Icons.bookmark,color:Colors.white),
-          Icon(Icons.add,color:Colors.white),
-          Icon(Icons.notifications,color:Colors.white),
-          Icon(Icons.person,color:Colors.white),
+        backgroundColor: sWhite,
+        color: sPrimary,
+        items: const [
+          Icon(Icons.home,color:sWhite),
+          Icon(Icons.bookmark,color:sWhite),
+          Icon(Icons.add,color:sWhite),
+          Icon(Icons.notifications,color:sWhite),
+          Icon(Icons.person,color:sWhite),
         ]
       ),
     );
